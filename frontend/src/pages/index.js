00d8a72b47a0e13fcea98cb9d87717cd719ba4e4
oiorain <marion.bouguet@gmail.com>
2025-05-16 16:26:03 +0200

    fix links

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -26,14 +26,14 @@ const Home = ({ }) => {
     <Layout>
       <div className="start">
         <img src="/start-screen-img.svg" />
-        <p>Suiren <button onClick={() => handleWordNavigation('睡蓮')} className="link-button">睡蓮</button> is a fun way to explore the Japanese language using Kanji to hop from one word to another, creating memorable connections. </p>
+        <p>Suiren <Link href="/word/睡蓮" onClick={() => handleWordNavigation('睡蓮')} className="link-button">睡蓮</Link> is a fun way to explore the Japanese language using Kanji to hop from one word to another, creating memorable connections. </p>
         <p>To start exploring, select a word below or search for one with at least one Kanji in it:</p>
         <div className="start-link-list">
-          <button onClick={() => handleWordNavigation('木')} className="start-link">木</button>
-          <button onClick={() => handleWordNavigation('大切')} className="start-link">大切</button>
-          <button onClick={() => handleWordNavigation('休息')} className="start-link">休息</button>
-          <button onClick={() => handleWordNavigation('満足')} className="start-link">満足</button>
-          <button onClick={() => handleWordNavigation('睡蓮')} className="start-link">睡蓮</button>
+          <Link href="/word/木" onClick={() => handleWordNavigation('木')} className="start-link link-button">木</Link>
+          <Link href="/word/大切" onClick={() => handleWordNavigation('大切')} className="start-link link-button">大切</Link>
+          <Link href="/word/休息" onClick={() => handleWordNavigation('休息')} className="start-link link-button">休息</Link>
+          <Link href="/word/満足" onClick={() => handleWordNavigation('満足')} className="start-link link-button">満足</Link>
+          <Link href="/word/睡蓮" onClick={() => handleWordNavigation('睡蓮')} className="start-link link-button">睡蓮</Link>
         </div>
         <p><small>Made by <a href="http://twitter.com/oiorain" target="_blank" rel="noopener noreferrer">Marion Kamoike-Bouguet</a> with <a href="http://aqworks.com" target="_blank" rel="noopener noreferrer">AQ</a>.<br/>
         Running on <a href='http://strapi.io' target='_blank' rel="noopener noreferrer">Strapi.io</a>, Hosted on <a href='https://strapi.io/cloud' target='_blank' rel="noopener noreferrer">StrapiCloud</a>.</small></p>
